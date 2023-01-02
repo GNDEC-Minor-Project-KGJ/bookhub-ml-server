daf555d8351a0ccda296e65dbe9b02add5908812
Konark Lohat <konarklohat123456@gmail.com>
2023-01-02 17:23:52 +0530

    APIs documented

--- api/urls.py
+++ api/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name="api-verview"),
-    path('book-list', views.ShowAll, name="list"),
-    path('book-detail/<int:id>', views.ShowOne, name="list-one"),
-    path('book-create', views.Create, name="create"),
-    path('book-update/<int:pk>', views.Update, name="update"),
-    path('book-delete/<int:pk>', views.Delete, name="delete"),
-    path('recommend-book-title/<str:genre>/<str:title>', views.recommend_by_title, name="recommend-by-title"),
-    path('recommend-book-desc/<str:genre>/<str:title>', views.recommend_by_desc, name="recommend-by-desc"),
-    path('top-rated/', views.top_rated, name="top-rated"),
-    path('longest/', views.longest, name="longest"),
+    path('book-list', views.ShowAll, name="list"), # List all the books in the database
+    path('book-detail/<int:id>', views.ShowOne, name="list-one"), # List one book in the database having the id
+    path('book-create', views.Create, name="create"), # Create a new book in the database
+    path('book-update/<int:pk>', views.Update, name="update"), # Update a book in the database having the id
+    path('book-delete/<int:pk>', views.Delete, name="delete"), # Delete a book in the database having the id
+    path('recommend-book-title/<str:genre>/<str:title>', views.recommend_by_title, name="recommend-by-title"), # Recommend a book by title
+    path('recommend-book-desc/<str:genre>/<str:title>', views.recommend_by_desc, name="recommend-by-desc"), # Recommend a book by description
+    path('top-rated/', views.top_rated, name="top-rated"), # List top rated books
+    path('longest/', views.longest, name="longest"), # List books with longest description
 ]
